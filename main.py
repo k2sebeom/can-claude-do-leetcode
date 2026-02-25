bb9ae682a5711d58d84e33602136e89903851831
k2sebeom <moses97@gmail.com>
2026-02-25 12:28:42 +0900

    feat: Use argparse in main

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import argparse
 import requests
-import json
 from models import LeetCodeProblem
+
 
 API_URL = "https://leetcode-api-pied.vercel.app/"
 
 
 def main():
-    resp = requests.get(API_URL + f"/problem/{1}")
+    parser = argparse.ArgumentParser(description="Fetch and save LeetCode problem")
+    parser.add_argument("problem_id", type=int, help="LeetCode problem ID")
+    args = parser.parse_args()
+
+    resp = requests.get(API_URL + f"/problem/{args.problem_id}")
     problem = LeetCodeProblem(**resp.json())
     print(problem.to_markdown())
 
@@ -14,10 +19,10 @@ def main():
 
     with open(f'dist/problems/{display_name}.md', 'w') as f:
         f.write(problem.to_markdown())
-    
+
     with open(f'dist/data/{display_name}.json', 'w') as f:
         f.write(problem.model_dump_json(indent=2))
-    
+
 
 if __name__ == "__main__":
     main()
